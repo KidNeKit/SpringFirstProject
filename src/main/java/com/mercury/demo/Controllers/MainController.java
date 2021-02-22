f54b33b7b0fd53ed9011100af7aeea2567d939b8
KidNeKit <kidnekit@mail.ru>
2021-02-22 15:29:20 +0300

    first commit

---
+++ src/main/java/com/mercury/demo/Controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.mercury.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "page");
+        return "home";
+    }
+
+}
